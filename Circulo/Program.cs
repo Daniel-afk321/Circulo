77c0d70bbcb3c6f584a42d3d3e5b02ec6d9a7679
Daniel-afk321 <83432335+Daniel-afk321@users.noreply.github.com>
2021-09-13 10:53:11 -0300

    Adicionar arquivos de projeto.

---
+++ Circulo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+
+namespace Circulo
+{
+    class Program
+    {
+        // valor fixo do Pi
+        static double Pi= 3.14;
+        static void Main(string[] args)
+        {
+            //entrada pro usuario
+            Console.WriteLine("Entre com o valor do raio: "); 
+            //declarando o valor do raio e oque ele vai receber do usuario
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            //valores da Circunferencia e do volume
+            double circ = Circuferencia(raio);
+            double volume = Volume(raio); 
+
+            // resposta para o usuario sobre a Circunferencia,volume e valor de PI
+            Console.WriteLine("Circuferência: " + circ.ToString("F2", CultureInfo.InvariantCulture)); 
+            Console.WriteLine("Volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor de PI: " + Pi.ToString("F2", CultureInfo.InvariantCulture));
+        }
+        // calculo da Circunferencia
+        static double Circuferencia(double r) {
+            return 2.0 * Pi * r; 
+        }
+        // calculo do Volume
+        static double Volume(double r) {
+            
+           return 4.0 / 3.0 * Pi * r * r * r;
+        }
+    }
+
+    } 
+
+
+
+
