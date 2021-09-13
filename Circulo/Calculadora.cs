0026846800eef9895e76c0f45bb59e6578733a7a
Daniel-afk321 <83432335+Daniel-afk321@users.noreply.github.com>
2021-09-13 11:09:38 -0300

    Criação de classe

---
+++ Circulo/Calculadora.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circulo
+{
+    class Calculadora
+    {
+
+        // valor fixo do Pi
+        public double Pi = 3.14;
+
+        // calculo da Circunferencia
+        public double Circuferencia(double r)
+        {
+            return 2.0 * Pi * r;
+        }
+        // calculo do Volume
+        public double Volume(double r)
+        {
+
+            return 4.0 / 3.0 * Pi * r * r * r;
+        }
+    }
+}
